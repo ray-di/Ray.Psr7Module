c3d6f2fa033c78a2476db2f19dfc28595b154f5d
Akihito Koriyama <akihito.koriyama@gmail.com>
2018-11-27 12:55:01 +0900

    bind UriInterface and UploadfilesProvider[] array bindings

--- src/Psr7Module.php
+++ src/Psr7Module.php
@@ -2,7 +2,9 @@
 namespace Ray\HttpMessage;
 
 use Nyholm\Psr7\ServerRequest;
+use Psr\Http\Message\UriInterface;
 use Ray\Di\AbstractModule;
+use Ray\HttpMessage\Annotation\UploadFiles;
 
 class Psr7Module extends AbstractModule
 {
@@ -10,5 +12,7 @@ class Psr7Module extends AbstractModule
     {
         $this->bind(RequestProviderInterface::class)->to(HttpRequestProvider::class);
         $this->bind(ServerRequest::class)->toProvider(HttpRequestRayProvider::class);
+        $this->bind(UriInterface::class)->toProvider(UriProvider::class);
+        $this->bind()->annotatedWith(UploadFiles::class)->toProvider(UploadfilesProvider::class);
     }
 }
--- tests/Psr7HttpModuleTest.php
+++ tests/Psr7HttpModuleTest.php
@@ -4,7 +4,10 @@ namespace Ray\HttpMessage;
 use Nyholm\Psr7\ServerRequest;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Message\UriInterface;
 use Ray\Di\Injector;
+use Ray\HttpMessage\Annotation\UploadFiles;
 
 class Psr7HttpModuleTest extends TestCase
 {
@@ -13,7 +16,7 @@ class Psr7HttpModuleTest extends TestCase
         $injector = new Injector(new Psr7Module);
         /* @var RequestProviderInterface $requestProvider */
         $requestProvider = $injector->getInstance(RequestProviderInterface::class);
-        $_SERVER = $this->superGlobalsServer();
+        $_SERVER = $this->superGlobalsServer() + $_SERVER;
         $request = $requestProvider->get();
         $this->assertInstanceOf(ServerRequestInterface::class, $request);
     }
@@ -26,6 +29,25 @@ class Psr7HttpModuleTest extends TestCase
         $this->assertInstanceOf(ServerRequest::class, $serverRequest);
     }
 
+    public function testPsr7UriTest()
+    {
+        $injector = new Injector(new Psr7Module);
+        /* @var UriInterface $uri */
+        $uri = $injector->getInstance(UriInterface::class);
+        $this->assertInstanceOf(UriInterface::class, $uri);
+    }
+
+    public function testPsr7UploadFilesTest()
+    {
+        $_FILES = $this->files();
+        $injector = new Injector(new Psr7Module);
+        $files = $injector->getInstance('', UploadFiles::class);
+        $file = $files['my-form']['details']['avatar'][2];
+        /* @var UploadedFileInterface $file */
+        $this->assertInstanceOf(UploadedFileInterface::class, $file);
+        $this->assertSame('my-avatar3.png', $file->getClientFilename());
+    }
+
     public function superGlobalsServer()
     {
         return  [
@@ -34,6 +56,59 @@ class Psr7HttpModuleTest extends TestCase
             'QUERY_STRING' => 'abc=123&foo=bar',
             'SERVER_NAME' => 'example.com',
             'CONTENT_TYPE' => 'multipart/form-data'
+        ];
+    }
+
+    public function files()
+    {
+        return [
+            'my-form' => [
+                'name' => [
+                    'details' => [
+                        'avatar' => [
+                            0 => 'my-avatar.png',
+                            1 => 'my-avatar2.png',
+                            2 => 'my-avatar3.png',
+                        ],
+                    ],
+                ],
+                'type' => [
+                    'details' => [
+                        'avatar' => [
+                            0 => 'image/png',
+                            1 => 'image/png',
+                            2 => 'image/png',
+                        ],
+                    ],
+                ],
+                'tmp_name' => [
+                    'details' => [
+                        'avatar' => [
+                            0 => 'phpmFLrzD',
+                            1 => 'phpV2pBil',
+                            2 => 'php8RUG8v',
+                        ],
+                    ],
+                ],
+                'error' => [
+                    'details' => [
+                        'avatar' => [
+                            0 => 0,
+                            1 => 0,
+                            2 => 0,
+                        ],
+                    ],
+                ],
+                'size' => [
+                    'details' => [
+                        'avatar' => [
+                            0 => 90996,
+                            1 => 90996,
+                            2 => 90996,
+                        ],
+                    ],
+                ],
+            ],
         ];
     }
 }
