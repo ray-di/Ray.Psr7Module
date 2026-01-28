2895a2523d8bc4085429fc251b053e02892076c8
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-01-28 23:50:28 +0900

    refactor: upgrade to PHP 8.1+, remove deprecated annotations
    - Require PHP ^8.1
    - Remove doctrine/annotations and koriym/attributes
    - Remove ray/aop from direct dependencies
    - Convert UploadFiles annotation to PHP 8 Attribute
    - Update LICENSE copyright year

--- src/Annotation/UploadFiles.php
+++ src/Annotation/UploadFiles.php
@@ -4,11 +4,6 @@ namespace Ray\HttpMessage\Annotation;
 use Attribute;
 use Ray\Di\Di\Qualifier;
 
-/**
- * @Annotation
- * @Target("METHOD")
- * @Qualifier
- */
 #[Attribute(Attribute::TARGET_METHOD), Qualifier]
 final class UploadFiles
 {
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,10 +2,4 @@
 
 declare(strict_types=1);
 
-use Koriym\Attributes\AttributeReader;
-use Ray\ServiceLocator\ServiceLocator;
-
-// no annotation in PHP 8
-if (PHP_MAJOR_VERSION >= 8) {
-    ServiceLocator::setReader(new AttributeReader());
-}
+// ServiceLocator::setReader(new AttributeReader());
