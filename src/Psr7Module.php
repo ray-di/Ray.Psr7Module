e946f88bd38234ee91ace22aa22e4fdc3d66906b
Akihito Koriyama <akihito.koriyama@gmail.com>
2026-01-28 23:18:44 +0900

    feat: bind ServerRequestInterface as singleton
    - Bind ServerRequestInterface, ServerRequest, UriInterface, and UploadFiles to Scope::SINGLETON
    - This prevents re-parsing of globals on every injection, improving performance

--- src/Psr7Module.php
+++ src/Psr7Module.php
@@ -8,6 +8,7 @@ use Nyholm\Psr7\ServerRequest;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\UriInterface;
 use Ray\Di\AbstractModule;
+use Ray\Di\Scope;
 use Ray\HttpMessage\Annotation\UploadFiles;
 
 class Psr7Module extends AbstractModule
@@ -15,9 +16,9 @@ class Psr7Module extends AbstractModule
     protected function configure()
     {
         $this->bind(RequestProviderInterface::class)->to(HttpRequestProvider::class);
-        $this->bind(ServerRequest::class)->toProvider(HttpRequestRayProvider::class);
-        $this->bind(ServerRequestInterface::class)->toProvider(HttpRequestRayProvider::class);
-        $this->bind(UriInterface::class)->toProvider(UriProvider::class);
-        $this->bind()->annotatedWith(UploadFiles::class)->toProvider(UploadfilesProvider::class);
+        $this->bind(ServerRequest::class)->toProvider(HttpRequestRayProvider::class)->in(Scope::SINGLETON);
+        $this->bind(ServerRequestInterface::class)->toProvider(HttpRequestRayProvider::class)->in(Scope::SINGLETON);
+        $this->bind(UriInterface::class)->toProvider(UriProvider::class)->in(Scope::SINGLETON);
+        $this->bind()->annotatedWith(UploadFiles::class)->toProvider(UploadfilesProvider::class)->in(Scope::SINGLETON);
     }
 }
